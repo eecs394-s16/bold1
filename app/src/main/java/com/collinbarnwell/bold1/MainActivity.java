e0fb3bd5680df081acc03122a85ff4f93888e24a
Shrivant Bhartia <shrivantbhartia@dhcp-10-105-144-23.wireless.northwestern.private>
2016-05-06 11:02:23 -0500

    hardcoded data with graph

--- app/src/main/java/com/collinbarnwell/bold1/MainActivity.java
+++ app/src/main/java/com/collinbarnwell/bold1/MainActivity.java
@@ -9,6 +9,10 @@ import android.view.View;
 import android.content.Intent;
 import android.view.View.OnClickListener;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -17,6 +21,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new OnClickListener(){
