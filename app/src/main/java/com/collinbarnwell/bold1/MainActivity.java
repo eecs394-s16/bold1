1cfcd646a70b43672477da9cbdc64309e5509221
collin <barnwell.collin@gmail.com>
2016-05-09 15:53:34 -0500

    graph data displaying, no x-axis labels

--- app/src/main/java/com/collinbarnwell/bold1/MainActivity.java
+++ app/src/main/java/com/collinbarnwell/bold1/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.format.DateFormat;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,19 +15,20 @@ import android.content.Intent;
 import android.view.View.OnClickListener;
 
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONObject;
 
-
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
 import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     public static final UtilClass utilClass = new UtilClass();
@@ -37,16 +39,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
-
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
-        });
-        graph.addSeries(series);
 
         Button prof_button = (Button) findViewById(R.id.profile_button);
         prof_button.setOnClickListener(new OnClickListener(){
@@ -61,8 +53,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, AddDataPoint.class));
             }
         });
-
-        refreshWelcomeMessage();
     }
 
     @Override
@@ -70,12 +60,12 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         // Whenever we go back to main activity, the name of the user may have changed (very unlikely)
         // No matter whether that happens, refresh welcome message.
-        refreshWelcomeMessage();
 
         DatabaseHelper mDbHelper = new DatabaseHelper(getBaseContext());
         SQLiteDatabase db = mDbHelper.getReadableDatabase();
 
         GraphView graph = (GraphView) findViewById(R.id.graph);
+
         LineGraphSeries<DataPoint> systolic_series =
                 new LineGraphSeries<DataPoint>(mDbHelper.getColumnDataPoints(db, "systolic_pressure"));
 
@@ -83,6 +73,21 @@ public class MainActivity extends AppCompatActivity {
         graph.getViewport().setScrollable(true);
         graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(MainActivity.this));
         graph.getGridLabelRenderer().setNumHorizontalLabels(4);
+
+        // Get one day ago
+        Calendar cal = Calendar.getInstance();
+        Date now = cal.getTime();
+        cal.add(Calendar.DAY_OF_YEAR, -1);
+        Date oneDayAgo = cal.getTime();
+
+        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(this));
+        graph.getGridLabelRenderer().setNumHorizontalLabels(4);
+        graph.getViewport().setMinX(oneDayAgo.getTime());
+        graph.getViewport().setMaxX(now.getTime());
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setScrollable(true);
+        // graph.getGridLabelRenderer().setGridStyle(GridLabelRenderer.GridStyle.BOTH);
+        // graph.getViewport().setScalable(true);
     }
 
 
@@ -106,24 +111,6 @@ public class MainActivity extends AppCompatActivity {
                 // Invoke the superclass to handle it.
                 return super.onOptionsItemSelected(item);
 
-        }
-    }
-
-    private void refreshWelcomeMessage(){
-        // Refresh welcome message
-        try{
-            // This is where we stored the user info
-            JSONObject saved_user_info=utilClass.loadJSONFromFile(this,utilClass.UserInfoFile);
-            if (saved_user_info.has(utilClass.UserInfoStrings[4]) && !(saved_user_info.getString(utilClass.UserInfoStrings[4]).isEmpty())){
-                // If there is a user first name field in local storage and it's not empty
-                ((TextView)findViewById(R.id.welcomeMessage)).setText("Welcome, "+saved_user_info.getString(utilClass.UserInfoStrings[4]));
-            }else{
-                ((TextView)findViewById(R.id.welcomeMessage)).setText(R.string.welcome_message_default);
-            }
-        }
-        catch(Exception e){
-            e.printStackTrace();
-            Toast.makeText(getBaseContext(),"Something went wrong while fetching user info.",Toast.LENGTH_LONG).show();
         }
     }
 }
