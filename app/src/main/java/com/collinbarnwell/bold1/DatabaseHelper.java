89456f0c680b14079a816fa5cdc18bf89d792e81
Shrivant Bhartia <shrivantbhartia@dhcp-10-105-116-207.wireless.northwestern.private>
2016-05-18 13:52:38 -0500

    wwwegew

--- app/src/main/java/com/collinbarnwell/bold1/DatabaseHelper.java
+++ app/src/main/java/com/collinbarnwell/bold1/DatabaseHelper.java
@@ -58,16 +58,25 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         return dataArray;
     }
 
-    public ArrayList<Double> getDataForPopup(SQLiteDatabase db, String timestamp) {
-        String query = "SELECT systolic_pressure, diastolic_pressure, heart_rate FROM data_point WHERE timestamp = " + timestamp + ";";
+    public Object[] getDataForPopup(SQLiteDatabase db, String timestamp) {
+        String query = "SELECT systolic_pressure, diastolic_pressure, heart_rate, mean_arterial_pressure, " +
+                "mood, exercise, tobacco, wake_up, food_intake, non_caffeine_fluid_intake, caffeine, " +
+                "about_to_sleep, daily_activity, other FROM data_point WHERE timestamp = " + timestamp + ";";
         Cursor cursor = db.rawQuery("query", null);
-        ArrayList<Double> data = new ArrayList<Double>();
-        Double sp = cursor.getDouble(cursor.getColumnIndex("systolic_pressure"));
-        Double dp = cursor.getDouble(cursor.getColumnIndex("diastolic_pressure"));
-        Double hr = cursor.getDouble(cursor.getColumnIndex("heart_rate"));
-        data.add(sp);
-        data.add(dp);
-        data.add(hr);
+        Object[] data = new Object[13];
+        data[0] = cursor.getDouble(cursor.getColumnIndex("systolic_pressure"));
+        data[1] = cursor.getDouble(cursor.getColumnIndex("diastolic_pressure"));
+        data[2] = cursor.getDouble(cursor.getColumnIndex("heart_rate"));
+        data[3] = cursor.getDouble(cursor.getColumnIndex("mean_arterial_pressure"));
+        data[4] = cursor.getString(cursor.getColumnIndex("mood"));
+        data[5] = cursor.getString(cursor.getColumnIndex("exercise"));
+        data[6] = cursor.getString(cursor.getColumnIndex("tobacco"));
+        data[7] = cursor.getString(cursor.getColumnIndex("wake_up"));
+        data[8] = cursor.getString(cursor.getColumnIndex("food_intake"));
+        data[9] = cursor.getString(cursor.getColumnIndex("non_caffeine_fluid_intake"));
+        data[10] = cursor.getString(cursor.getColumnIndex("caffeine"));
+        data[11] = cursor.getString(cursor.getColumnIndex("about_to_sleep"));
+        data[12] = cursor.getString(cursor.getColumnIndex("daily_activity"));
         return data;
     }
 
