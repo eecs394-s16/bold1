de685137c411e3b427ba35dd0f456ed8abb6a3aa
Zavier Henry <zavierhenry2012@u.northwestern.edu>
2016-05-23 12:45:53 -0500

    "Initial start of notifications GUI"

---
+++ app/src/main/java/com/collinbarnwell/bold1/Notifications.java
@@ -0,0 +1,62 @@
+package com.collinbarnwell.bold1;
+
+import com.collinbarnwell.bold1.DatabaseHelper;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ViewFlipper;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import android.widget.Toast;
+import android.widget.DatePicker;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+
+import java.util.Calendar;
+import java.util.Date;
+
+
+
+
+/**
+ * Created by Z-Henry on 5/23/2016.
+ */
+public class Notifications extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+
+        //setContentView(R.layout.activity_notifications);
+        
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
